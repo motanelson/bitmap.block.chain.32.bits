aad793decdd06f0e3067513142919d11880e4b4c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-02-21 12:25:47 +0000

    Add files via upload

--- shell.js
+++ shell.js
@@ -6,17 +6,16 @@ console.log('\033c\033[43;30m\n');
 var t=true
 var a=""
 var inputString=""
-for(i=0;i<1000;i++){
+
 const rl = readline.createInterface({ input });
 
     rl.question('> : ', (answer) => {
-    
-
-    rl.close();
-   });
-   answer="ls"
-   var w =cds.exec( "/usr/bin/"+answer  , function (err , out ,se){
+    for(i=0;i<1000;i++){
+    var w =cds.exec( "/usr/bin/"+answer  , function (err , out ,se){
 	     console.log(`${out}\n`);
              if (err!=null) console.log(`${err}`);
          });
-}
+    };
+    rl.close();
+   });
+      
